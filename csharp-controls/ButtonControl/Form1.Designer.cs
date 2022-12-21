de74aa027f0cb51faa64b056bfdf474b83c8d9f0
Ugur Coruh <ugur.coruh.tr@gmail.com>
2022-12-21 15:49:31 +0300

    Button Generation Example Added

---
+++ csharp-controls/ButtonControl/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ButtonControl
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGenerateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonGenerateButton
+            // 
+            this.buttonGenerateButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonGenerateButton.Location = new System.Drawing.Point(654, 12);
+            this.buttonGenerateButton.Name = "buttonGenerateButton";
+            this.buttonGenerateButton.Size = new System.Drawing.Size(134, 56);
+            this.buttonGenerateButton.TabIndex = 0;
+            this.buttonGenerateButton.Text = "Generate Buttons";
+            this.buttonGenerateButton.UseVisualStyleBackColor = true;
+            this.buttonGenerateButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonGenerateButton);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGenerateButton;
+    }
+}
+
---
+++ csharp-controls/ButtonControl/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ButtonControl
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            buttonGenerateButton.Text= "OK";
+
+            int buttonWidth = 50;
+            int buttonHeight = 100;
+            int buttonColumnSize = 15;
+            int buttonRowSize = 3;
+
+
+            for (int i = 0; i < buttonColumnSize; i++)
+            {
+                for (int j = 0; j < buttonRowSize; j++)
+                {
+                    Button newButton = new Button();
+                    newButton.Enabled = true;
+                    newButton.Location = new Point(i * buttonWidth + buttonColumnSize, j* buttonHeight + buttonWidth);
+                    newButton.Name = i.ToString()+"_"+j.ToString();
+                    newButton.Text = "Button-" + (i + 1).ToString() +","+ (j + 1).ToString();
+                    newButton.Size = new Size(buttonWidth, buttonHeight);
+                    newButton.Tag = i.ToString() + "_" + j.ToString();
+                    newButton.Click += NewButton_Click;
+                    
+
+                    this.Controls.Add(newButton);
+
+                }
+
+            }
+        }
+
+        private void NewButton_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            MessageBox.Show("Button-"+button.Tag + " Clicked!!!");
+
+        }
+    }
+}
