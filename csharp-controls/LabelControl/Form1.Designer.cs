22b19a1f1ec870eaa8cabd7acd392d8880631852
Ugur Coruh <ugur.coruh.tr@gmail.com>
2022-12-21 15:18:56 +0300

    background worker control tested

---
+++ csharp-controls/LabelControl/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace LabelControl
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.buttonStartWorker = new System.Windows.Forms.Button();
+            this.buttonStopWorker = new System.Windows.Forms.Button();
+            this.richtextLog = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // buttonStartWorker
+            // 
+            this.buttonStartWorker.Location = new System.Drawing.Point(37, 25);
+            this.buttonStartWorker.Name = "buttonStartWorker";
+            this.buttonStartWorker.Size = new System.Drawing.Size(169, 54);
+            this.buttonStartWorker.TabIndex = 0;
+            this.buttonStartWorker.Text = "START WORKER";
+            this.buttonStartWorker.UseVisualStyleBackColor = true;
+            this.buttonStartWorker.Click += new System.EventHandler(this.buttonStartWorker_Click);
+            // 
+            // buttonStopWorker
+            // 
+            this.buttonStopWorker.Location = new System.Drawing.Point(37, 113);
+            this.buttonStopWorker.Name = "buttonStopWorker";
+            this.buttonStopWorker.Size = new System.Drawing.Size(169, 54);
+            this.buttonStopWorker.TabIndex = 1;
+            this.buttonStopWorker.Text = "STOP WORKER";
+            this.buttonStopWorker.UseVisualStyleBackColor = true;
+            this.buttonStopWorker.Click += new System.EventHandler(this.buttonStopWorker_Click);
+            // 
+            // richtextLog
+            // 
+            this.richtextLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richtextLog.Location = new System.Drawing.Point(260, 25);
+            this.richtextLog.Name = "richtextLog";
+            this.richtextLog.Size = new System.Drawing.Size(432, 164);
+            this.richtextLog.TabIndex = 2;
+            this.richtextLog.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(716, 201);
+            this.Controls.Add(this.richtextLog);
+            this.Controls.Add(this.buttonStopWorker);
+            this.Controls.Add(this.buttonStartWorker);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Label Sample Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button buttonStartWorker;
+        private System.Windows.Forms.Button buttonStopWorker;
+        private System.Windows.Forms.RichTextBox richtextLog;
+    }
+}
+
---
+++ csharp-controls/LabelControl/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace LabelControl
+{
+    public partial class MainForm : Form
+    {
+        bool _flag = true;
+
+        public MainForm()
+        {
+
+            CheckForIllegalCrossThreadCalls = false;
+
+            InitializeComponent();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while(_flag)
+            {
+                richtextLog.AppendText("Hello World\r\n");
+                Thread.Sleep(2000);
+            }
+        }
+
+        private void buttonStartWorker_Click(object sender, EventArgs e)
+        {
+            _flag = true;
+
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void buttonStopWorker_Click(object sender, EventArgs e)
+        {
+            _flag= false;
+        }
+    }
+}
