6ae55ea5c56ac634be4cd2280bb3d203fa60946e
Ugur Coruh <ugur.coruh.tr@gmail.com>
2022-12-23 14:10:47 +0300

    CheckboxControl dynamic event addition and removing operation example added with forecolor change use case

---
+++ csharp-controls/CheckboxControl/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace CheckboxControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkboxMovieTypeDrama = new System.Windows.Forms.CheckBox();
+            this.checkboxMovieTypeScienceFiction = new System.Windows.Forms.CheckBox();
+            this.checkboxMovieTypeAction = new System.Windows.Forms.CheckBox();
+            this.groupboxMovieTypes = new System.Windows.Forms.GroupBox();
+            this.groupboxMovieTypes.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkboxMovieTypeDrama
+            // 
+            this.checkboxMovieTypeDrama.AutoSize = true;
+            this.checkboxMovieTypeDrama.Location = new System.Drawing.Point(28, 32);
+            this.checkboxMovieTypeDrama.Name = "checkboxMovieTypeDrama";
+            this.checkboxMovieTypeDrama.Size = new System.Drawing.Size(94, 29);
+            this.checkboxMovieTypeDrama.TabIndex = 0;
+            this.checkboxMovieTypeDrama.Text = "Drama";
+            this.checkboxMovieTypeDrama.UseVisualStyleBackColor = true;
+            this.checkboxMovieTypeDrama.CheckedChanged += new System.EventHandler(this.checkboxMovieTypeDrama_CheckedChanged);
+
+            // 
+            // checkboxMovieTypeScienceFiction
+            // 
+            this.checkboxMovieTypeScienceFiction.AutoSize = true;
+            this.checkboxMovieTypeScienceFiction.Location = new System.Drawing.Point(28, 79);
+            this.checkboxMovieTypeScienceFiction.Name = "checkboxMovieTypeScienceFiction";
+            this.checkboxMovieTypeScienceFiction.Size = new System.Drawing.Size(174, 29);
+            this.checkboxMovieTypeScienceFiction.TabIndex = 1;
+            this.checkboxMovieTypeScienceFiction.Text = "Science Fiction";
+            this.checkboxMovieTypeScienceFiction.UseVisualStyleBackColor = true;
+
+            // 
+            // checkboxMovieTypeAction
+            // 
+            this.checkboxMovieTypeAction.AutoSize = true;
+            this.checkboxMovieTypeAction.Location = new System.Drawing.Point(28, 123);
+            this.checkboxMovieTypeAction.Name = "checkboxMovieTypeAction";
+            this.checkboxMovieTypeAction.Size = new System.Drawing.Size(91, 29);
+            this.checkboxMovieTypeAction.TabIndex = 2;
+            this.checkboxMovieTypeAction.Text = "Action";
+            this.checkboxMovieTypeAction.UseVisualStyleBackColor = true;
+
+            // 
+            // groupboxMovieTypes
+            // 
+            this.groupboxMovieTypes.Controls.Add(this.checkboxMovieTypeDrama);
+            this.groupboxMovieTypes.Controls.Add(this.checkboxMovieTypeAction);
+            this.groupboxMovieTypes.Controls.Add(this.checkboxMovieTypeScienceFiction);
+            this.groupboxMovieTypes.Font = new System.Drawing.Font("Roboto", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupboxMovieTypes.Location = new System.Drawing.Point(74, 50);
+            this.groupboxMovieTypes.Name = "groupboxMovieTypes";
+            this.groupboxMovieTypes.Size = new System.Drawing.Size(227, 172);
+            this.groupboxMovieTypes.TabIndex = 3;
+            this.groupboxMovieTypes.TabStop = false;
+            this.groupboxMovieTypes.Text = "Select Movie";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupboxMovieTypes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupboxMovieTypes.ResumeLayout(false);
+            this.groupboxMovieTypes.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkboxMovieTypeDrama;
+        private System.Windows.Forms.CheckBox checkboxMovieTypeScienceFiction;
+        private System.Windows.Forms.CheckBox checkboxMovieTypeAction;
+        private System.Windows.Forms.GroupBox groupboxMovieTypes;
+    }
+}
+
---
+++ csharp-controls/CheckboxControl/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckboxControl
+{
+    public partial class Form1 : Form
+    {
+        private bool _checkboxDramaEventFlag = false;
+        private bool _checkboxActionEventFlag = false;
+        private bool _checkboxScienceFictionEventFlag = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.checkboxMovieTypeDrama.MouseEnter += new System.EventHandler(this.checkboxMovieType_MouseEnter);
+            this.checkboxMovieTypeDrama.MouseLeave += new System.EventHandler(this.checkboxMovieType_MouseLeave);
+
+            _checkboxDramaEventFlag = true;
+
+            this.checkboxMovieTypeScienceFiction.MouseEnter += new System.EventHandler(this.checkboxMovieType_MouseEnter);
+            this.checkboxMovieTypeScienceFiction.MouseLeave += new System.EventHandler(this.checkboxMovieType_MouseLeave);
+
+            _checkboxActionEventFlag = true;
+        
+            this.checkboxMovieTypeAction.MouseEnter += new System.EventHandler(this.checkboxMovieType_MouseEnter);
+            this.checkboxMovieTypeAction.MouseLeave += new System.EventHandler(this.checkboxMovieType_MouseLeave);
+
+            _checkboxScienceFictionEventFlag = true;
+    }
+
+        private void checkboxMovieType_MouseEnter(object sender, EventArgs e)
+        {
+            CheckBox checkboxControl = (CheckBox)sender;
+            checkboxControl.ForeColor = Color.Red;
+
+            if(checkboxControl.Name.ToUpper().Contains("DRAMA"))
+            {
+                if (_checkboxActionEventFlag) {
+                    this.checkboxMovieTypeAction.MouseEnter -= new System.EventHandler(this.checkboxMovieType_MouseEnter);
+                    this.checkboxMovieTypeAction.MouseLeave -= new System.EventHandler(this.checkboxMovieType_MouseLeave);
+                    _checkboxActionEventFlag = false;
+                }
+
+            }
+            else if (checkboxControl.Name.ToUpper().Contains("SCİENCE"))
+            {
+                if (!_checkboxActionEventFlag)
+                {
+                    this.checkboxMovieTypeAction.MouseEnter  += new System.EventHandler(this.checkboxMovieType_MouseEnter);
+                    this.checkboxMovieTypeAction.MouseLeave  += new System.EventHandler(this.checkboxMovieType_MouseLeave);
+                    _checkboxActionEventFlag = true;
+                }
+            }
+        }
+
+        private void checkboxMovieType_MouseLeave(object sender, EventArgs e)
+        {
+            CheckBox checkboxControl = (CheckBox)sender;
+            checkboxControl.ForeColor = Color.Black;
+        }
+
+        private void checkboxMovieTypeDrama_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
