a16272d0c4df5df6f13cd7316f51a8c9cae20378
Ugur Coruh <ugur.coruh.tr@gmail.com>
2022-12-23 15:00:53 +0300

    CheckedListboxControl addition removing and selection example added

---
+++ csharp-controls/CheckedListboxControl/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace CheckedListboxControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBoxRegisteredPeople = new System.Windows.Forms.CheckedListBox();
+            this.textboxName = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.checkedListBoxSelectedPeople = new System.Windows.Forms.CheckedListBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textboxSurname = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textboxEid = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkedListBoxRegisteredPeople
+            // 
+            this.checkedListBoxRegisteredPeople.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkedListBoxRegisteredPeople.FormattingEnabled = true;
+            this.checkedListBoxRegisteredPeople.Location = new System.Drawing.Point(3, 16);
+            this.checkedListBoxRegisteredPeople.Name = "checkedListBoxRegisteredPeople";
+            this.checkedListBoxRegisteredPeople.Size = new System.Drawing.Size(741, 307);
+            this.checkedListBoxRegisteredPeople.TabIndex = 0;
+            this.checkedListBoxRegisteredPeople.SelectedIndexChanged += new System.EventHandler(this.checkedListBoxRegisteredPeople_SelectedIndexChanged);
+            // 
+            // textboxName
+            // 
+            this.textboxName.Location = new System.Drawing.Point(64, 16);
+            this.textboxName.Name = "textboxName";
+            this.textboxName.Size = new System.Drawing.Size(117, 20);
+            this.textboxName.TabIndex = 1;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(12, 109);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(169, 23);
+            this.buttonAdd.TabIndex = 4;
+            this.buttonAdd.Text = "ADD";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(12, 149);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(169, 23);
+            this.buttonDelete.TabIndex = 5;
+            this.buttonDelete.Text = "DELETE";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // checkedListBoxSelectedPeople
+            // 
+            this.checkedListBoxSelectedPeople.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkedListBoxSelectedPeople.FormattingEnabled = true;
+            this.checkedListBoxSelectedPeople.Location = new System.Drawing.Point(3, 16);
+            this.checkedListBoxSelectedPeople.Name = "checkedListBoxSelectedPeople";
+            this.checkedListBoxSelectedPeople.Size = new System.Drawing.Size(967, 309);
+            this.checkedListBoxSelectedPeople.TabIndex = 4;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(18, 16);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 7;
+            this.labelName.Text = "Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Surname";
+            // 
+            // textboxSurname
+            // 
+            this.textboxSurname.Location = new System.Drawing.Point(64, 42);
+            this.textboxSurname.Name = "textboxSurname";
+            this.textboxSurname.Size = new System.Drawing.Size(117, 20);
+            this.textboxSurname.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkedListBoxSelectedPeople);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(973, 328);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Selected Person List";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.checkedListBoxRegisteredPeople);
+            this.groupBox2.Location = new System.Drawing.Point(3, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(747, 326);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Registered Person List";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.labelName);
+            this.groupBox3.Controls.Add(this.textboxEid);
+            this.groupBox3.Controls.Add(this.buttonDelete);
+            this.groupBox3.Controls.Add(this.textboxName);
+            this.groupBox3.Controls.Add(this.buttonAdd);
+            this.groupBox3.Controls.Add(this.textboxSurname);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Location = new System.Drawing.Point(753, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(208, 314);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Recording";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "EID";
+            // 
+            // textboxEid
+            // 
+            this.textboxEid.Location = new System.Drawing.Point(64, 68);
+            this.textboxEid.Name = "textboxEid";
+            this.textboxEid.Size = new System.Drawing.Size(117, 20);
+            this.textboxEid.TabIndex = 3;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox3);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(973, 664);
+            this.splitContainer1.SplitterDistance = 332;
+            this.splitContainer1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(973, 664);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBoxRegisteredPeople;
+        private System.Windows.Forms.TextBox textboxName;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.CheckedListBox checkedListBoxSelectedPeople;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textboxSurname;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textboxEid;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+    }
+}
+
---
+++ csharp-controls/CheckedListboxControl/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckedListboxControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string generateRecordString(string name, string surname, string eid)
+        {
+            return name + " " + surname + " [" + eid + "]";
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            string record = generateRecordString(textboxName.Text, textboxSurname.Text, textboxEid.Text);
+
+            checkedListBoxRegisteredPeople.Items.Add(record);
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (checkedListBoxRegisteredPeople.CheckedItems.Count > 0)
+            {
+                for (int i = checkedListBoxRegisteredPeople.CheckedItems.Count - 1; i >= 0; i--)
+                {
+                    checkedListBoxRegisteredPeople.Items.Remove(checkedListBoxRegisteredPeople.CheckedItems[i]);
+                }
+
+            }
+        }
+
+        private void checkedListBoxRegisteredPeople_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            checkedListBoxSelectedPeople.Items.Clear();
+
+            for (int i = 0; i < checkedListBoxRegisteredPeople.CheckedItems.Count; i++)
+            {
+                checkedListBoxSelectedPeople.Items.Add(checkedListBoxRegisteredPeople.CheckedItems[i]);
+            }
+            
+        }
+    }
+}
